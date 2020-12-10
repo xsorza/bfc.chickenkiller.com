32338b855256770861e2b4c9c46d1d11b71470b2
/cRo22 D3M0n <65735449+xsorza@users.noreply.github.com>
2020-12-11 07:18:01 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+<body>
+<title> CTF CHALLANGE </title>
+<text> <h5> <center> halo om tolong bantu aku.!!! </center> </h5> </text>
+<text> <center> saya bertemu seorang petani..dan dia sangat ramah </text>
+<text> <center> tetapi dia sangat <h5> [warp.txt] </h5> dan sayapun kaget:) </text>
+<body> <center> jika Kaka menolongku hariku akan bahagia dan Kaka juga </center> </body>
+<head>
+</head>
+</body>
+</html>
