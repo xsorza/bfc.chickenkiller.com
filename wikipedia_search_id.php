c3049c47e856ba8829b2e9899b59a9e9fda8b218
/cRo22 D3M0n <65735449+xsorza@users.noreply.github.com>
2020-12-11 06:30:58 +0800

    Create wikipedia_search_id.php

---
+++ wikipedia_search_id.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=0.85, user-scalable=no">
+      <meta name="og:author" content="Xsorza">
+      <meta content="HTML, CSS, JavaScript" meta name="keywords"> 
+      <meta content="text/html;charset=UTF-8" http-equiv="Content-Type"> 
+      <meta content="public" http-equiv="Cache-Control"> 
+      <meta content="no-cache" http-equiv="Pragma"> 
+      <meta content="Big Family The Death Note" name="description"> 
+      <meta content="Wikipedia Search ID Tools" property="og:title">
+      <link rel="icon" type="image/png" href="https://d.top4top.io/p_1644hs3tu1.jpg"><link rel="shortcut icon">
+      <meta property="og:image" content="https://d.top4top.io/p_1644hs3tu1.jpg">
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+      <script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>
+<title>Wikipedia Search ID Tools</title> 
+<style>
+
+  img[alt="www.000webhost.com"]{display:none;}
+
+</style>
+<style>
+
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: Share Tech Mono;
+    user-select: none;
+    outline: none;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: linear-gradient(to right, rgb(90, 20, 20), rgb(10, 8, 110));
+  }
+
+  #kimochi{
+    width:100%;
+    padding:20px 0;
+  }
+
+  .uwu {
+    justify-content: center;
+    align-items: center;
+    min-height: 600px;
+    background: black;
+    border-top: 4px solid red;
+    border-bottom: 4px solid blue;
+    border-left: 4px solid green;
+    border-right: 4px solid yellow;
+    width: 380px;
+  }
+     
+  .sec {
+    justify-content: center;
+    align-items: center;
+    min-height: 600px;
+    background: black;
+    border: 5px dotted silver;
+    width: 330px;
+  }
+  
+  .form-control {
+    width: 225px;
+    border: none;
+    background: transparent;
+    outline: none;
+    padding: 10px 10px;
+    font-size: 16px;
+    box-sizing: border-box;
+    border: 1px solid silver;
+    color: silver;
+  }
+  
+  .btn {
+    padding: 10px 10px;
+    background: transparent;
+    outline: none;
+    color: silver;
+    border: 1px solid silver;
+    font-size: 16px;
+    position: relative;
+    cursor: pointer;
+    display: inline-block;
+  }
+        
+  .btn:hover {
+    color: red;
+    border: 1px solid !important;
+    transition: all 0.5s ease;
+  }
+          
+  a {
+    font-size: 18px;
+    color: lime;
+    text-decoration: none;
+  } 
+          
+  a:hover {color: yellow;} 
+  
+  .blink {
+    animation: blink-animation 0.5s steps(5, start) infinite;
+    -webkit-animation: blink-animation 0.5s steps(5, start) infinite;
+  }
+
+  @keyframes blink-animation {
+    to {
+    visibility: hidden;
+  }
+  }
+
+  @-webkit-keyframes blink-animation {
+    to {
+    visibility: hidden;
+  }
+  }
+
+</style>
+</head>
+<body>
+<body onload="initpg();">
+  <center><br><br>
+<div class="uwu"><br>
+  <div class="sec"><br>
+  <hr size="6" color="silver"><br>
+  <p style='margin: 0px 30px;'><font color='#fff' size='5'>INFORMATION USER</font><br><br></p><p style='margin: 0px 30px;'><font color='#fff' size='2'>IP User : 114.79.37.241</font><br></p><p style='margin: 0px 30px;'><font color='#fff' size='2'>Browser : Chrome</font><br></p><p style='margin: 0px 30px;'><font color='#fff' size='2'>Sistem Operasi : Mozilla/5.0 (Linux; Android 10; CPH2209) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36<br>
+  Real Time :  <b>10-Dec-2020</b> | <b>05:23:59 </b><br><br>
+    <center>
+      <hr size="6" color="silver"><br>
+      <hr size="2" color="white"><br>
+      <font color="red" size="6"  class="blink">&nbsp;TOOLS </font>
+      <font color="white" size="6"  class="blink">ONLINE</font><br>
+    <font size="5">
+            <script>
+                //<![CDATA[
+
+                var text = "&nbsp; Xsorza"
+                var speed = 80;
+
+                if (document.all || document.getElementById) {
+                    document.write('<span id="highlight">' + text + '</span>')
+                    var storetext = document.getElementById ? document.getElementById("highlight") : document.all.highlight
+                } else
+                    document.write(text)
+                var hex = new Array("00", "14", "28", "3C", "50", "64", "78", "8C", "A0", "B4", "C8", "DC", "F0")
+                var r = 1
+                var g = 1
+                var b = 1
+                var seq = 1
+
+                function changetext() {
+                    rainbow = "#" + hex[r] + hex[g] + hex[b]
+                    storetext.style.color = rainbow
+                }
+
+                function change() {
+                    if (seq == 6) {
+                        b--
+                        if (b == 0)
+                            seq = 1
+                    }
+                    if (seq == 5) {
+                        r++
+                        if (r == 12)
+                            seq = 6
+                    }
+                    if (seq == 4) {
+                        g--
+                        if (g == 0)
+                            seq = 5
+                    }
+                    if (seq == 3) {
+                        b++
+                        if (b == 12)
+                            seq = 4
+                    }
+                    if (seq == 2) {
+                        r--
+                        if (r == 0)
+                            seq = 3
+                    }
+                    if (seq == 1) {
+                        g++
+                        if (g == 12)
+                            seq = 2
+                    }
+                    changetext()
+                }
+
+                function starteffect() {
+                    if (document.all || document.getElementById)
+                        flash = setInterval("change()", speed)
+                }
+                starteffect()
+
+                //]]>
+            </script>
+        </font>
+    </center>
+      <br><hr size="2" color="white"><br>
+      <hr size="6" color="silver"><br>
+  <tr>
+    <td>
+      <hr size="2" color="yellow"><br><br>
+        <center>
+            <form method="post">
+                <input type="text" name="ip" class="form-control" placeholder="Masukan Kata">
+                <input type="submit" name="Enter" class='btn' value="Enter">
+            </form>
+        </center>
+                <br>
+        <font color="silver" size="5">Hasil</font><br><br>
+        <p style='margin: 0px 30px;'><font color=silver size=3></font></p>
+        <br><br><hr size="2" color="yellow"><br>
+    </td>
+  </tr>
+        <hr size="6" color="silver"><br>
+  </div><br>
+</div><br><br>
+   </center>
+</body>
+<script>
+
+  var rev = "fwd";
+  function titlebar(val)
+  {
+  var msg = "Wikipedia Search ID Tools";
+  var res = " ";
+  var speed = 30;
+  var pos = val;   
+  msg = " " +msg+" ";
+  var le = msg.length;
+  if(rev == "fwd"){
+  if(pos < le){
+  pos = pos+1;
+  scroll = msg.substr(0,pos);
+  document.title = scroll;
+  timer = window.setTimeout("titlebar("+pos+")",speed);
+  }
+  else{
+  rev = "bwd";
+  timer = window.setTimeout("titlebar("+pos+")",speed);
+  }
+  }
+  else{
+  if(pos > 0){
+  pos = pos-1;
+  var ale = le-pos;
+  scrol = msg.substr(ale,le);
+  document.title = scrol;
+  timer = window.setTimeout("titlebar("+pos+")",speed);
+  }
+  else{
+  rev = "fwd";
+  timer = window.setTimeout("titlebar("+pos+")",speed);
+  } 
+  }
+  }       
+  titlebar(0);
+
+</script>
+</html>
